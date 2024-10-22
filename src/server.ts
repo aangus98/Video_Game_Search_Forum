a366cb14ed7070475a427d631b3510d844e73fba
Andrew <mrangus298@gmail.com>
2024-10-21 19:19:06 -0500

    Fixed up the server, userRoutes, and authroutes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import sequelize from './config/connection.js';
-import routes from './routes/index.js';
-
+import { userRoutes } from './routes/api/userRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 app.use(express.static('../client/dist'));
 app.use(express.json());
-app.use(routes);
+app.use(userRoutes);
 
 // Connect to the database before starting the Express.js server
 sequelize.sync().then(() => {
