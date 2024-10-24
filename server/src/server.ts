21274028551947ca34213dda63c4057b1fbb5979
Andrew <mrangus298@gmail.com>
2024-10-24 17:58:06 -0500

    Fixed up code to match with main

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,4 @@
-// import sequelize from './config/connection.js';
-// import userRoutes from './routes/userRoutes.js';
 import express from 'express';
-import sequelize from './config/connection.js';
-import { userRoutes } from './routes/api/userRoutes.js';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import cors from 'cors';
@@ -15,15 +11,13 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 
 app.use(express.json());
-app.use(userRoutes);
-
 
 app.post('/api/search', async (req, res) => {
   const {query} = req.body;
   try {
     const response = await axios.post(
       'https://api.igdb.com/v4/games',
-      `fields name; search "${query}";`,
+      `fields name, cover.image_id, aggregated_rating, first_release_date, genres.name, involved_companies.company.name ; search "${query}"; limit 1;`,
       {
         headers: {
           'Accept': 'application/json',
@@ -32,7 +26,17 @@ app.post('/api/search', async (req, res) => {
         }
       }
     );
-    res.json(response.data);
+
+const gameData = response.data.map(({aggregated_rating, involved_companies, first_release_date, genres, cover, ...rest }) => ({
+  ...rest,
+  cover: `https://images.igdb.com/igdb/image/upload/t_cover_big/${cover.image_id}.jpg`,
+  critic_score: Math.round(aggregated_rating),
+  genres: genres.map(genreObj => genreObj.name),
+  developers: involved_companies.map(companyObj => companyObj.company.name),
+  release_date: first_release_date ? new Date(first_release_date * 1000).toLocaleDateString() : "Unknown",
+}));
+
+    res.json(gameData);
   } catch (error) {
     console.error('Something went horribly wrong!!!!:', error);
     res.status(500).json({message: 'Big Ole Server Error'});
@@ -42,23 +46,3 @@ app.post('/api/search', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// app.use(express.static('../client/dist'));
-// app.use(express.json());
-// app.use(routes);
-
-// //Register API routes
-// app.use('/api', userRoutes); //Add user routes
-
-// // Connect to the database before starting the Express.js server
-// sequelize.sync()
-//   .then(() => {
-//     console.log('Connected to database successfully.');
-//     app.listen(PORT, () => {
-//       console.log(`Server is listening on port ${PORT}`);
-//     });
-//   })
-//   .catch((err: Error) => {
-//     console.error('Failed to sync database:', err);
-//     process.exit(1);
-//   });
